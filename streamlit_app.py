8864dcffb27d73a334aec1c58cb441fee514fc4a
lsmerpthangesh-max <lsmerpthangesh@gmail.com>
2026-06-03 14:48:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ body {
 
 /* 🖤 TEXT → BLACK (MAIN FIX) */
 h1, h2, h3, h4, h5, h6, p, label, div, span {
-    color: #ED5D04 !important;
+    color: #FC8625 !important;
 }
 h1 {
     color: unset !important;
